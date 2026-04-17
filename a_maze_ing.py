60c7e5c26961a0056f95766011b07c16aa2f55ae
Adrien COMBIER <adrien.combier21@gmail.com>
2026-04-17 11:28:18 +0200

    Phase 0 finish

---
+++ a_maze_ing.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def main():
+    if not len(sys.argv) == 2:
+        print("Usage: python3 a_maze_ing.py <config_file>")
+        sys.exit(1)
+    config_file = sys.argv[1]
+    print(f"{config_file}")
+
+
+if __name__ == "__main__":
+    main()
